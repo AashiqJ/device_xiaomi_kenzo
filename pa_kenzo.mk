b7252236ed2129526ed22da40b732b4fd7c3de37
AashiqJ <aashiqjacob96@gmail.com>
2017-12-09 11:28:54 +0530

    Update and rename aoscp.mk to pa_kenzo.mk

--- pa_kenzo.mk
+++ pa_kenzo.mk
@@ -1,10 +1,12 @@
-# Copyright 2017 Android Open Source Project
+#
+# Copyright 2016 The CyanogenMod Project
+# Copyright 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,27 +15,38 @@
 # limitations under the License.
 #
 
+# Check for target product
+ifeq (pa_kenzo,$(TARGET_PRODUCT))
+
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
+# Set bootanimation to 1080p display.
+TARGET_BOOT_ANIMATION_RES := 1080
 
-# Inherit some common AOSCP stuff.
-$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
+# Inherit from our common CAF device tree.
+include device/qcom/common/common.mk
 
+# Inherit the main configuration from our vendor.
+include vendor/pa/main.mk
+
+TARGET_WANTS_EXTENDED_DPM_PLATFORM := true
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := aoscp_kenzo
+PRODUCT_NAME := pa_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Use the latest approved GMS identifiers unless running a signed build
-ifneq ($(SIGN_BUILD),true)
+TARGET_VENDOR := Xiaomi
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V8.2.1.0.MHOCNDL:user/release-keys \
     PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V8.2.1.0.MHOCNDL release-keys"
+
+
 endif
